eae6ab08a4710dca87e118c15928dbb0a5d974c5
PSJahn <66626848+PSJahn@users.noreply.github.com>
2022-10-29 18:23:18 +0200

    Added all the files

---
+++ BuildToolsGUI/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace BuildToolsGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 37);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "show file on finish";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 117);
+            this.richTextBox1.MaxLength = 10;
+            this.richTextBox1.Multiline = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(131, 23);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "1.19.2";
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Minecraft Version (ex: 1.19.2)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(15, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "BUILD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Update BuildTools";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::BuildToolsGUI.Properties.Resources.clowncorpsmall;
+            this.ClientSize = new System.Drawing.Size(155, 288);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "BuildToolsGUI";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BuildToolsGUI/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuildToolsGUI
+{
+    public partial class Form1 : Form
+    {
+        public static bool BuildToolsDetected = true;
+        public Form1()
+        {
+            string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string strWorkPath = Path.GetDirectoryName(strExeFilePath);
+            if(!File.Exists(strWorkPath+"\\BuildTools.jar")) { BuildToolsDetected = false; }
+            InitializeComponent();
+            if(!BuildToolsDetected)
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string buildToolsPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\BuildTools.jar";
+            if(File.Exists(buildToolsPath))
+            {
+                File.Delete(buildToolsPath);
+            }
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    client.Headers.Add("User-Agent: Other");
+                    client.DownloadFile(@"https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar", buildToolsPath);
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show("Error occurred on download:\n" + ex.Message);
+            }
+            button2.Text = "Complete!";
+            button2.ForeColor = Color.Green;
+            BuildToolsDetected = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.WorkingDirectory = path;
+            processStartInfo.FileName = "cmd.exe";
+            processStartInfo.Arguments = "/C java -jar BuildTools.jar --rev " + richTextBox1.Text;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = true;
+            Process p = new Process();
+            p.StartInfo = processStartInfo;
+            button1.Text = "BUILDING...";
+            button1.ForeColor = Color.Black;
+            this.Update();
+            button1.Update();
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            string errorOutput = p.StandardError.ReadToEnd();
+            p.WaitForExit();
+            if(errorOutput.Contains("Could not get version"))
+            {
+                MessageBox.Show("Invalid Version Selected!\nValid Version Example: 1.19.2");
+                button1.Text = "FAILED";
+                button1.ForeColor = Color.Red;
+                return;
+            }
+            button1.Text = "BUILT!";
+            if(checkBox1.Checked)
+            {
+                Process.Start(path);
+                Environment.Exit(0);
+            }
+            button1.ForeColor = Color.Green;
+        }
+    }
+}
